75ff1a87f1fab76ba3f75bd47dcf7e4b7d6442c5
Lucas Martins <lucasowza@hotmail.com>
2025-03-15 03:51:33 -0400

    feat: implement user registration use case with validation and password hashing

---
+++ apps/api/src/modules/identity/application/use-cases/register-user.ts
@@ -0,0 +1,50 @@
+import { Either, left, right } from "@/@shared/core/either";
+import { User } from "../../domain/entities/user";
+import { UserAlreadyExistsException } from "../exceptions/UserAlreadyExistsException";
+import { UserRepository } from "../ports/repositories/user.repository";
+import { HasherService } from "../ports/services/hasher-service";
+
+export type RegisterUserUseCaseRequest = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type RegisterUserUseCaseResponse = Either<
+  UserAlreadyExistsException,
+  {
+    user: User;
+  }
+>;
+
+export class RegisterUserUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly hashService: HasherService
+  ) {}
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUserUseCaseRequest): Promise<RegisterUserUseCaseResponse> {
+    const userWithSameEmail = await this.userRepository.exists(email);
+
+    if (userWithSameEmail) {
+      return left(new UserAlreadyExistsException(email));
+    }
+
+    const hashedPassword = await this.hashService.hash(password);
+
+    const user = User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await this.userRepository.save(user);
+
+    return right({
+      user,
+    });
+  }
+}
