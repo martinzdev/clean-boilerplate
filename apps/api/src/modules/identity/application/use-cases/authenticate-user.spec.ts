745c71a845771fbdb9879aa174302636426d4277
Lucas Martins <lucasowza@hotmail.com>
2025-03-15 03:52:03 -0400

    test: add tests for user authentication use case

---
+++ apps/api/src/modules/identity/application/use-cases/authenticate-user.spec.ts
@@ -0,0 +1,43 @@
+import { makeUser } from "test/factories/make-user";
+import { InMemoryUserRepository } from "test/repositories/in-memory-user.repository";
+import { FakeEncrypter } from "test/services/fake-encrypter.service";
+import { FakeHasher } from "test/services/fake-hasher.service";
+import { AuthenticateUserUseCase } from "./authenticate-user";
+
+let inMemoryUserRepository: InMemoryUserRepository;
+let fakeHasher: FakeHasher;
+let fakeEncrypter: FakeEncrypter;
+let sut: AuthenticateUserUseCase;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    fakeHasher = new FakeHasher();
+    fakeEncrypter = new FakeEncrypter();
+    sut = new AuthenticateUserUseCase(
+      inMemoryUserRepository,
+      fakeHasher,
+      fakeEncrypter
+    );
+  });
+
+  it("should be able to authenticate user", async () => {
+    const user = makeUser({
+      email: "johndoe@gmail.com",
+      password: await fakeHasher.hash("123456"),
+    });
+
+    await inMemoryUserRepository.save(user);
+
+    const result = await sut.execute({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    });
+  });
+});
