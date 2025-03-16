6beb566f688b21a3749253692aa9259dae2737cf
Lucas Martins <lucasowza@hotmail.com>
2025-03-16 18:24:18 -0400

    feat: refactor TypeORM module and add user and session entities

--- apps/api/src/infra/database/typeorm/typeorm.module.ts
+++ apps/api/src/infra/database/typeorm/typeorm.module.ts
@@ -1,17 +1,25 @@
 import { EnvModule } from "@/infra/env/env.module";
+
 import { EnvService } from "@/infra/env/env.service";
+
 import { Module } from "@nestjs/common";
+
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { SessionEntity } from "./session.entity";
-import { UserEntity } from "./user.entity";
+
+import { SessionEntity } from "./entities/session.entity";
+
+import { UserEntity } from "./entities/user.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [EnvModule],
+
       inject: [EnvService],
+
       useFactory(env: EnvService) {
         const databaseUrl = env.get("DATABASE_URL");
+        const schema = env.get("DATABASE_SCHEMA");
 
         return {
           type: "postgres",
@@ -19,11 +27,14 @@ import { UserEntity } from "./user.entity";
           entities: [UserEntity, SessionEntity],
           synchronize: true,
           logging: true,
+          schema: process.env.DATABASE_SCHEMA || schema,
         };
       },
     }),
+
     TypeOrmModule.forFeature([UserEntity, SessionEntity]),
   ],
+
   exports: [TypeOrmModule],
 })
 export class TypeOrmConfigModule {}
